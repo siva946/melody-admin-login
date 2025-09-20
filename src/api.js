b3864b53da22f8c0ba89ba70ad80a8ef2fcba413
siva946 <123248824+siva946@users.noreply.github.com>
2025-09-20 20:29:23 +0530

    api changes

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL ||  "http://localhost:5000/api" });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL ||  "https://melody-admin-backend.vercel.app/api" });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
